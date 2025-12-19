33df0e3ba4a57f6990964a62608bc6fb29c6b850
Niks Klava <niksklava@gmail.com>
2025-12-19 17:07:06 +0200

    ritenis klase izveidota

--- Klava/Ritenis.java
+++ Klava/Ritenis.java
@@ -1,5 +1,15 @@
 package Klava;
 
 public class Ritenis {
-
+    private String tips;
+    private int izmērs;
+    
+    public Ritenis(String tips, int izmērs, int pozīcija) {
+        this.tips = tips;
+        this.izmērs = izmērs;
+    }
+    
+    public String getApraksts() {
+        return String.format("Ritenis \nTips: "+ tips + "\nIzmērs: " + izmērs);
+    }
 }
