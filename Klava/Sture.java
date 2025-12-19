cf5e2773bc1bab488756deb6b3f216132d074eb0
Niks Klava <niksklava@gmail.com>
2025-12-19 17:03:41 +0200

    stures klase izveidota

--- Klava/Sture.java
+++ Klava/Sture.java
@@ -1,5 +1,17 @@
 package Klava;
 
 public class Sture {
-
+    String tips;
+    
+    public Sture(String tips) {
+        this.tips = tips;
+    }
+    
+    public void pagriezt(String virziens) {
+        System.out.println("Stūre pagriezta " + virziens);
+    }
+    
+    public String getApraksts() {
+        return String.format("STŪRE\nTips: " + tips);
+    }
 }
