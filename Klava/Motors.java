a7b8284fe7e61a6375fc95a71c4387b6dd26194d
Niks Klava <niksklava@gmail.com>
2025-12-19 17:00:24 +0200

    motora klases izveide

--- Klava/Motors.java
+++ Klava/Motors.java
@@ -1,5 +1,21 @@
 package Klava;
 
 public class Motors {
-
+    double tilpums;
+    int jauda;
+    String tips;
+    
+    public Motors(double tilpums, int jauda, String tips) {
+        this.tilpums = tilpums;
+        this.jauda = jauda;
+        this.tips = tips;
+    }
+    
+    public void startet() {
+        System.out.println("Motors startēts!");
+    }
+    
+    public String getApraksts() {
+        return String.format("MOTORS\nTilpums: " + tilpums + "\nJauda: " + jauda + "\nTips: " + tips);
+    }
 }
